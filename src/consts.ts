a30ffde537afbd4977b7786a53cc9dbd26a016aa
jguimeram <jguimera54@gmail.com>
2025-09-26 16:15:25 +0200

    change site title

--- src/consts.ts
+++ src/consts.ts
@@ -1,11 +1,11 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "JGM Blog";
+export const SITE_DESCRIPTION = "Welcome to my website!";
 export const MENU = [
-    { title: 'Home', href: '/' },
-    { title: 'Blog', href: '/blog' },
-    { title: 'About', href: '/about' },
-    { title: 'Contact', href: '/contact' },
+  { title: "Home", href: "/" },
+  { title: "Blog", href: "/blog" },
+  { title: "About", href: "/about" },
+  { title: "Contact", href: "/contact" },
 ];
